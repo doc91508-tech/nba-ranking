ada57f3cee0c11dbc8526ec678c78f422fd533ff
doc91508-tech <doc91508@gmail.com>
2026-07-26 21:01:43 +0800

    更新说明

---
+++ server.py
@@ -0,0 +1,10 @@
+﻿import http.server, os
+class H(http.server.SimpleHTTPRequestHandler):
+    def guess_type(self, path):
+        ctype = super().guess_type(path)
+        if ctype == "text/html":
+            return "text/html; charset=utf-8"
+        return ctype
+import socketserver
+with socketserver.TCPServer(("0.0.0.0", 3000), H) as httpd:
+    httpd.serve_forever()
